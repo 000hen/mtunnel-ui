96fa6437c859aad55281cad593bf1ff2a473bdb8
000hen <git@muisnowdevs.one>
2025-12-05 22:20:14 +0800

    [fix] update build command to include correct path for mtunnel

--- scripts/build-sidecar.js
+++ scripts/build-sidecar.js
@@ -28,7 +28,7 @@ function build(goos, goarch, outfile) {
         CGO_ENABLED: "0",
     };
 
-    const cmd = `go build -ldflags "-s -w" -o ${output} ${projectDir}`;
+    const cmd = `go build -ldflags "-s -w" -o ${output} ${projectDir}/cmd/mtunnel/`;
 
     run(cmd, env);
     console.log("Built:", output);
